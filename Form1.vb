db348078e87b6df263bcf1864dc4c1b563973493
Nailah Berube <berube_nailah@columbusstate.edu>
2023-02-14 23:07:30 -0500

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,30 @@
+﻿Public Class frmNumbers
+    Private Sub lblInstructions_TextChanged(sender As Object, e As EventArgs) Handles lblInstructions.TextChanged
+
+    End Sub
+
+    Private Sub lblFrench_Invisible(sender As Object, e As EventArgs) Handles MyBase.Load
+        lblFrench.Visible = False
+
+
+    End Sub
+
+    Private Sub lblFrench_Visible(sender As Object, e As EventArgs) Handles btnOne.Click, btnTwo.Click, btnThree.Click, btnFour.Click, btnFive.Click
+        lblFrench.Text = "Un"
+        lblFrench.Text = "Deux"
+        lblFrench.Text = "Trois"
+        lblFrench.Text = "Quatre"
+        lblFrench.Text = "Cinq"
+
+    End Sub
+
+    Private Sub Close_Form(sender As Object, e As EventArgs) Handles btnExit.Click
+
+        Application.Exit()
+
+    End Sub
+
+    Private Sub lblFrench_TextChanged(sender As Object, e As EventArgs) Handles lblFrench.TextChanged
+
+    End Sub
+End Class
